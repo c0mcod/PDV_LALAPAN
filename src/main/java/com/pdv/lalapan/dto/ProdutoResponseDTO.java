093b513e2a5c1c4fa42b57992b6b938b81e1edbe
comcod <netosilvaneto6@gmail.com>
2026-01-20 18:03:30 -0300

    adicionado campo de quantidade em estoque para validação

--- src/main/java/com/pdv/lalapan/dto/ProdutoResponseDTO.java
+++ src/main/java/com/pdv/lalapan/dto/ProdutoResponseDTO.java
@@ -6,9 +6,9 @@ import com.pdv.lalapan.enums.Unidade;
 
 import java.math.BigDecimal;
 
-public record ProdutoResponseDTO(Long id, String codigo, String nome, BigDecimal Preco, Unidade unidade, Categoria categoria) {
+public record ProdutoResponseDTO(Long id, String codigo, double quantidadeEstoque,  String nome, BigDecimal Preco, Unidade unidade, Categoria categoria) {
 
     public ProdutoResponseDTO(Produto entity) {
-        this(entity.getId(), entity.getCodigo(), entity.getNome(), entity.getPreco(), entity.getUnidade(), entity.getCategoria());
+        this(entity.getId(), entity.getCodigo(), entity.getQuantidadeEstoque(), entity.getNome(), entity.getPreco(), entity.getUnidade(), entity.getCategoria());
     }
 }
