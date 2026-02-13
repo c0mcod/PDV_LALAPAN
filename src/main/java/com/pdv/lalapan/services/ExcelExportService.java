cb12c06d0ceb38f5414d6351604fbe06e0c31d23
comcod <netosilvaneto6@gmail.com>
2026-02-13 16:12:32 -0300

    Ajuste para exportação de valor de compra para excel

--- src/main/java/com/pdv/lalapan/services/ExcelExportService.java
+++ src/main/java/com/pdv/lalapan/services/ExcelExportService.java
@@ -22,11 +22,12 @@ public class ExcelExportService {
         headerRow.createCell(0).setCellValue("CODIGO");
         headerRow.createCell(1).setCellValue("NOME");
         headerRow.createCell(2).setCellValue("CATEGORIA");
-        headerRow.createCell(3).setCellValue("QUANTIDADE");
-        headerRow.createCell(4).setCellValue("UNIDADE");
-        headerRow.createCell(5).setCellValue("QTD. MIN");
-        headerRow.createCell(6).setCellValue("VALOR");
-        headerRow.createCell(7).setCellValue("STATUS");
+        headerRow.createCell(3).setCellValue("PREÇO DE CUSTO");
+        headerRow.createCell(4).setCellValue("QUANTIDADE");
+        headerRow.createCell(5).setCellValue("UNIDADE");
+        headerRow.createCell(6).setCellValue("QTD. MIN");
+        headerRow.createCell(7).setCellValue("VALOR");
+        headerRow.createCell(8).setCellValue("STATUS");
 
         int rowNum = 1;
         for(Produto produto : produtos) {
@@ -34,11 +35,12 @@ public class ExcelExportService {
             row.createCell(0).setCellValue(produto.getCodigo());
             row.createCell(1).setCellValue(produto.getNome());
             row.createCell(2).setCellValue(produto.getCategoria().toString());
-            row.createCell(3).setCellValue(produto.getQuantidadeEstoque().doubleValue());
-            row.createCell(4).setCellValue(produto.getUnidade().toString());
-            row.createCell(5).setCellValue(produto.getEstoqueMinimo().doubleValue());
-            row.createCell(6).setCellValue(produto.getPreco().doubleValue());
-            row.createCell(7).setCellValue(produto.getAtivo() ? "Ativo" : "Inativo");
+            row.createCell(3).setCellValue(produto.getPrecoCusto().doubleValue());
+            row.createCell(4).setCellValue(produto.getQuantidadeEstoque().doubleValue());
+            row.createCell(5).setCellValue(produto.getUnidade().toString());
+            row.createCell(6).setCellValue(produto.getEstoqueMinimo().doubleValue());
+            row.createCell(7).setCellValue(produto.getPreco().doubleValue());
+            row.createCell(8).setCellValue(produto.getAtivo() ? "Ativo" : "Inativo");
         }
 
         for(int i = 0; i < 6; i++) {
