02cf5bcd38b92f1ed3d0dc8cbea9bff4d28f30f6
c0mcod <netosilvaneto6@gmail.com>
2026-03-12 14:20:33 -0300

    Correção de exception

--- src/main/java/com/pdv/lalapan/exceptions/UsuarioNaoEncontradoException.java
+++ src/main/java/com/pdv/lalapan/exceptions/UsuarioNaoEncontradoException.java
@@ -1,10 +1,10 @@
 package com.pdv.lalapan.exceptions;
 
 public class UsuarioNaoEncontradoException extends RuntimeException {
-  private Long usuarioId;
+  private final Long usuarioId;
 
     public UsuarioNaoEncontradoException(Long usuarioId) {
-        super(String.format("Usuario com id: %d nao encontrado."));
+        super(String.format("Usuario com id: %d nao encontrado.", usuarioId));
         this.usuarioId = usuarioId;
     }
 
